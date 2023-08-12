47fa9c1a3c5974e489b7d5ab790982ae835b16f5
Emilie Hopkinson <emilie.n.hopkinson.22@dartmouth.edu>
2023-08-12 17:20:45 -0400

    toast password matching works!

--- layered/src/pages/auth/Register.js
+++ layered/src/pages/auth/Register.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
 
     const[email, SetEmail] = useState("");
     const[password, SetPassword] = useState("");
     const[cPassword, SetCPassword] = useState("");
-
+    const fail = () => toast("Passwords do not match");
+    const success = () => toast("Successfully registered");
+    
     const registerUser = (e) => {
         e.preventDefault();
 
-        
         console.log(email, password, cPassword);
+
+        if(password !== cPassword){
+            fail();
+        } else {
+            success();
+        }
 
         SetEmail('');
         SetPassword('');
@@ -24,8 +33,9 @@ const Register = () => {
                 <input type='text' placeholder='Email' required value={email} onChange={(e) => SetEmail(e.target.value)}/>
                 <input type='text' placeholder='Password' required value={password} onChange={(e) => SetPassword(e.target.value)}/>
                 <input type='text' placeholder='Confirm Password' required value={cPassword} onChange={(e) => SetCPassword(e.target.value)}/>
-                <button type="submit"> submit </button>
+                <button type="submit" > submit </button>
             </form>
+            <ToastContainer/>
         </div>
     )
 }
