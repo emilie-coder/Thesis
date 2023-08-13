2d8e6fc92ae9685f975065d47eb54e36bd5661bb
Emilie Hopkinson <emilie.n.hopkinson.22@dartmouth.edu>
2023-08-13 10:37:07 -0400

    logout working

--- layered/src/components/header/Header.js
+++ layered/src/components/header/Header.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import headerCSS from './Header.module.css';
+import { getAuth, signOut } from "firebase/auth";
+import Loader from "../../components/loader/Loader";
+import { useNavigate } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
 
 const Header = () => {
+
+  const[isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+
+  const logoutUser = () =>{
+    const auth = getAuth();
+  signOut(auth).then(() => {
+    toast.success("Logged out :D");
+    setIsLoading(false);
+  }).catch((error) => {
+    toast.error(error)
+    setIsLoading(false);
+  });
+  }
+
+
   return (
     <div className={headerCSS.header}>
+      {isLoading && <Loader/>}
+      <ToastContainer/>
       <nav>
         <ul className={headerCSS.ul}>
           <Link to='/'> Layered </Link>
@@ -13,6 +37,7 @@ const Header = () => {
           <Link to='/Contact'> Contact </Link>
           <Link to='/Login'> Login </Link>
           <Link to='/Register'> Register </Link>
+          <Link to='/' onClick={logoutUser}> Log Out </Link>
         </ul>
       </nav>
     </div>
