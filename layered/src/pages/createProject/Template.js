18d5e76c193a1b25bc1763a21cef8879abb63674
Emilie Hopkinson <emilie.n.hopkinson.22@dartmouth.edu>
2023-08-16 01:38:12 -0400

    uploading images work!

--- layered/src/pages/createProject/Template.js
+++ layered/src/pages/createProject/Template.js
@@ -2,15 +2,38 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import { selectProjectAuthor, selectProjectID, selectProjectTemplate, selectProjectTitle } from '../../redux/slice/projectSlice';
 import ThreeScene from "../../threejs/three-scene";
-
+import { storage } from '../../firebase/config';
+import { useState } from 'react';
+import { selectUserID } from '../../redux/slice/authSlice'
+import { v4 } from "uuid";
+import { ref, uploadBytes } from 'firebase/storage';
 
 const Template = () => {
 
 
   const projAuthor = useSelector(selectProjectAuthor);
-    const projiD = useSelector(selectProjectID);
-    const proTemplate = useSelector(selectProjectTemplate);
-    const projTitle = useSelector(selectProjectTitle);
+  const projID = useSelector(selectProjectID);
+  const proTemplate = useSelector(selectProjectTemplate);
+  const projTitle = useSelector(selectProjectTitle);
+
+  const userID = useSelector(selectUserID);
+
+  const uploadImage = () => {
+    if(imageUpload == null) return;
+
+    const imageRef = ref(storage, userID + '/project_' + projID + `/images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then(() => {
+      alert("image upload");
+    })
+
+  };
+
+
+
+  const [imageUpload, setImageUplad] = useState(null);
+
+
+
 
     return (
         <div>
@@ -21,7 +44,7 @@ const Template = () => {
                     title: {projTitle}
                 </div>
                 <div>
-                    projiD: {projiD}
+                    projiD: {projID}
                 </div>
                 <div>
                     proTemplate: {proTemplate}
@@ -32,6 +55,8 @@ const Template = () => {
             </div>
 
             <div>
+              <input type="file" onChange={((event) => {setImageUplad(event.target.files[0])})}/>
+              <button onClick={uploadImage}>upload image</button>
               {/* <ThreeScene/> */}
             </div>
         </div>
